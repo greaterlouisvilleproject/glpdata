f9ac6f152b099892d5a72c8cf27f9bf0164455b2
mraisle <mraisle@live.unc.edu>
2021-04-07 12:39:59 -0400

    disconnected map data - add muw,nh, and tract files and modify disconnected script

--- data-raw/qop/disconnected.R
+++ data-raw/qop/disconnected.R
@@ -21,6 +21,44 @@ acs_micro %<>%
 disconnected_county  <- survey_by_demog(acs_micro, "disconnected")
 disconnected_msa_1yr <- survey_by_demog(acs_micro, "disconnected", geog = "MSA")
 
-usethis::use_data(disconnected_county, disconnected_msa_1yr, overwrite = TRUE)
+#Map making from ACS5
+disconnected_vars_05_5yr   <- build_census_var_df("acs5", "B14005")
 
-rm(acs_micro)
+#total columns = "B14005_001E", "B14005_002E", "B14005_016E"
+#identify specific vars we want
+keep_all <- c("B14005_003E", "B14005_009E", "B14005_013E", "B14005_017E",
+                    "B14005_023E", "B14005_027E", "B14005_010E", "B14005_011E", "B14005_014E", "B14005_015E",
+                    "B14005_024E", "B14005_025E", "B14005_028E", "B14005_029E",
+                    "B14005_003M", "B14005_009M", "B14005_013M", "B14005_017M",
+                    "B14005_023M", "B14005_027M", "B14005_010M", "B14005_011M", "B14005_014M", "B14005_015M",
+                    "B14005_024M", "B14005_025M", "B14005_028M", "B14005_029M")
+disconnected <- c("B14005_010E" ,"B14005_011E", "B14005_014E", "B14005_015E",
+                  "B14005_024E", "B14005_025E", "B14005_028E", "B14005_029E", "B14005_010M", "B14005_011M", "B14005_014M", "B14005_015M",
+                  "B14005_024M", "B14005_025M", "B14005_028M", "B14005_029M")
+
+justgood <- disconnected_vars_05_5yr %>%
+  subset(variable %in% keep_all)
+
+#get data
+youth <- get_census(justgood, "tract")
+
+#make categorical variable
+youth_2 <- youth  %>%
+  mutate(disconnect = if_else(variable %in% disconnected, T, F))
+
+#process census
+clean_disconnected <- youth_2 %>%
+  process_census(cat_var = "disconnect",
+                 output_name = "disconnected")
+
+#process map
+map_disconnected <- clean_disconnected%>%
+  process_map(disconnected, return_name = "disconnected") %>%
+  list2env(.GlobalEnv)
+
+usethis::use_data(disconnected_county, disconnected_msa_1yr,disconnected_nh, disconnected_muw, disconnected_tract, overwrite = TRUE)
+
+rm(acs_micro, clean_disconnected, justgood, youth, youth_2, disconnected_vars_05_5yr)
+
+
+
