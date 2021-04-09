c54e9f3a5398a1d875435190e86b64d912b0ab78
Harrison <harrisonk.kirby@gmail.com>
2021-04-09 12:04:22 -0400

    messy certificate file

--- data-raw/education/certificates.R
+++ data-raw/education/certificates.R
@@ -1,39 +1,35 @@
-library(tidyr)
-library(readr)
-library(dplyr)
-library(stringr)
-library(magrittr)
 library(glptools)
+glp_load_packages()
 
-library(feather)
-library(survey)
-
-cps_micro <- read_feather("data-raw/microdata/cps_micro.feather")
+cps_micro <- feather::read_feather("data-raw/microdata/cps_micro.feather")
 
 cps_micro %<>%
+  filter(
+    age %in% 25:64,
+    !duplicated(CPSIDP)) %>%
   mutate(
-    cert = if_else(PROFCERT == 2, 1, 0),
-    cert = replace(cert, PROFCERT == 99, NA),
-    cert_degree = if_else(cert == 1 | educ %in% c("assoc", "bach", "grad"), 1, 0),
-    cert_no_degree = if_else(cert == 1 & educ %in% c("no_hs", "hs", "some_col"), 1, 0)) %>%
-  select(-PROFCERT) %>%
-  filter(age %in% 25:64) %>%
-  filter(!duplicated(CPSIDP))
+    certificate = if_else(PROFCERT == 2, 1, 0),
+    certificate = if_else(PROFCERT == 99, NA_real_, certificate),
+    certificate_degree = if_else(certificate == 1 | educ %in% c("assoc", "bach", "grad"), 1, 0),
+    certificate_no_degree = if_else(certificate == 1 & educ %in% c("no_hs", "hs", "some_col"), 1, 0))
 
-cps_svy <- svydesign(ids = ~1, weights = ~WTFINL, data = cps_micro)
+cps_svy <- survey::svydesign(ids = ~1, weights = ~WTFINL, data = cps_micro)
 
-certificates_msa_1yr <- svyby(~cert+cert_degree+cert_no_degree,
-                          ~MSA+year, design = cps_svy, svymean, na.rm = TRUE,
-                          na.rm.all = TRUE, drop.empty.groups = FALSE)
+certificate_msa_1yr <-
+  survey::svyby(~certificate+certificate_degree+certificate_no_degree,
+                ~MSA+year, design = cps_svy, survey::svymean,
+                na.rm = TRUE, na.rm.all = TRUE, drop.empty.groups = FALSE)
 
-certificates_msa_1yr %<>%
-  select(-se.cert, -se.cert_degree, -se.cert_no_degree) %>%
-  mutate_at(vars(MSA, year), unfactor) %>%
-  mutate_at(vars(cert:cert_no_degree), ~ . * 100) %>%
+certificate_msa_1yr %<>%
+  select(-se.certificate, -se.certificate_degree, -se.certificate_no_degree) %>%
+  mutate(
+    MSA = as.character(MSA),
+    year = as.numeric(as.character(year))) %>%
+  mutate_at(vars(certificate:certificate_no_degree), ~ . * 100) %>%
   mutate(sex = "total", race = "total", MSA = as.character(MSA)) %>%
-  filter(year < 2019) %>%
+  filter(year > 2014, year < 2020) %>%
   organize()
 
-update_sysdata(certificates_msa_1yr)
+usethis::use_data(certificate_msa_1yr, overwrite = TRUE)
 
 rm(cps_micro, cps_svy)
