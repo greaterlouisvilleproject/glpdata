a96dd9494c7361a46521ba9c7859bfd275fb988b
Harrison <harrisonk.kirby@gmail.com>
2021-04-09 12:14:49 -0400

    Terry Tonlan's Child Poverty graph

--- data-raw/education/poverty.R
+++ data-raw/education/poverty.R
@@ -1,301 +1,117 @@
-library(tidyr)
-library(readr)
-library(dplyr)
-library(stringr)
-library(magrittr)
 library(glptools)
+glp_load_packages()
 
-path <- "data-raw/education/poverty/"
+poverty_vars_00_total <- build_census_var_df("sf3", "PCT049", additional_filters = "below|above")
+poverty_vars_00_race  <- build_census_var_df("sf3", "PCT075", additional_filters = "below|above")
+poverty_vars_05_1yr   <- build_census_var_df("acs1", "B17001", additional_filters = "below|above")
+poverty_vars_05_5yr   <- build_census_var_df("acs5", "B17001", additional_filters = "below|above")
 
-process_00  <- function(df){
-  df %<>%
-    transmute(
-      FIPS = as.character(as.numeric(Id2)),
-      year = 2000,
+poverty_vars_1yr <- bind_rows(poverty_vars_00_total, poverty_vars_00_race, poverty_vars_05_1yr)
+poverty_vars_5yr <- bind_rows(poverty_vars_00_total, poverty_vars_00_race, poverty_vars_05_5yr)
 
-      num_pov_under_5.male =
-        `Income in 1999 below poverty level: - Male: - Under 5 years`,
-      num_pov_under_5.female =
-        `Income in 1999 below poverty level: - Female: - Under 5 years`,
-      num_pov_under_5 =
-        num_pov_under_5.male + num_pov_under_5.female,
+poverty_county            <- get_census(poverty_vars_1yr, "FIPS")
+#poverty_all_counties_5yr <- get_census(poverty_vars_5yr, "FIPS")
+poverty_map               <- get_census(poverty_vars_5yr, "tract")
+#poverty_zip               <- get_census(filter(poverty_vars_5yr, year >= 2010), "zip")
 
-      num_pov_5_17.male =
-        `Income in 1999 below poverty level: - Male: - 5 years` +
-        `Income in 1999 below poverty level: - Male: - 6 to 11 years` +
-        `Income in 1999 below poverty level: - Male: - 12 to 14 years` +
-        `Income in 1999 below poverty level: - Male: - 15 years` +
-        `Income in 1999 below poverty level: - Male: - 16 and 17 years`,
-      num_pov_5_17.female =
-        `Income in 1999 below poverty level: - Female: - 5 years` +
-        `Income in 1999 below poverty level: - Female: - 6 to 11 years` +
-        `Income in 1999 below poverty level: - Female: - 12 to 14 years` +
-        `Income in 1999 below poverty level: - Female: - 15 years` +
-        `Income in 1999 below poverty level: - Female: - 16 and 17 years`,
-      num_pov_5_17 =
-        num_pov_5_17.male + num_pov_5_17.female,
+poverty_county %<>% mutate(poverty_status = if_else(str_detect(label, "below"), T, F))
+poverty_map    %<>% mutate(poverty_status = if_else(str_detect(label, "below"), T, F))
+#poverty_zip    %<>% mutate(poverty_status = if_else(str_detect(label, "below"), T, F))
 
-      num_pov_child.male =
-        num_pov_under_5.male + num_pov_5_17.male,
-      num_pov_child.female =
-        num_pov_under_5.female + num_pov_5_17.female,
-      num_pov_child =
-        num_pov_under_5 + num_pov_5_17,
+poverty_county %<>% process_census(cat_var = "poverty_status",
+                                   output_name = "poverty",
+                                   age_groups = c("all", "0_4", "0_17"))
 
-      num_pov.male =
-        `Income in 1999 below poverty level: - Male:`,
-      num_pov.female =
-        `Income in 1999 below poverty level: - Female:`,
-      num_pov =
-        num_pov.male + num_pov.female,
+poverty_map %<>% process_census(cat_var = "poverty_status",
+                                output_name = "poverty",
+                                age_groups = c("all", "0_4", "0_17"))
 
-      total_under_5.male =
-        num_pov_under_5.male +
-        `Income in 1999 at or above poverty level: - Male: - Under 5 years`,
-      total_under_5.female =
-        num_pov_under_5.female +
-        `Income in 1999 at or above poverty level: - Female: - Under 5 years`,
-      total_under_5 =
-        total_under_5.male + total_under_5.female,
+#Create zip-code poverty data?
+if(FALSE){
+   test1 <- glpdata:::population_zip_full_MSA %>%
+      group_by(zip, year, sex, race, population_total) %>%
+      summarise(
+         population_in_MSA = sum(population_in_FIPS), .groups = "drop")
 
-      total_5_17.male =
-        num_pov_5_17.male +
-        `Income in 1999 at or above poverty level: - Male: - 5 years` +
-        `Income in 1999 at or above poverty level: - Male: - 6 to 11 years` +
-        `Income in 1999 at or above poverty level: - Male: - 12 to 14 years` +
-        `Income in 1999 at or above poverty level: - Male: - 15 years` +
-        `Income in 1999 at or above poverty level: - Male: - 16 and 17 years`,
-      total_5_17.female =
-        num_pov_5_17.female +
-        `Income in 1999 at or above poverty level: - Female: - 5 years` +
-        `Income in 1999 at or above poverty level: - Female: - 6 to 11 years` +
-        `Income in 1999 at or above poverty level: - Female: - 12 to 14 years` +
-        `Income in 1999 at or above poverty level: - Female: - 15 years` +
-        `Income in 1999 at or above poverty level: - Female: - 16 and 17 years`,
-      total_5_17 =
-        total_5_17.male + total_5_17.female,
+   test2 <- poverty_zip %>%
+      process_census(cat_var = "poverty_status",
+                     output_name = "poverty")
 
-      total_child.male =
-        total_under_5.male + total_5_17.male,
-      total_child.female =
-        total_under_5.female + total_5_17.female,
-      total_child =
-        total_child.male + total_child.female,
+   test4 <- glptools:::FIPS_zip_full_MSA %>% filter(pct_pop_in_county >= 50)
 
-      total.male =
-        num_pov.male +
-        `Income in 1999 at or above poverty level: - Male:`,
-      total.female =
-        num_pov.female +
-        `Income in 1999 at or above poverty level: - Female:`,
-      total =
-        total.male + total.female)
+   test3 <- test2 %>%
+      left_join(MSA_zip, by = "zip") %>%
+      filter(MSA == "31140", sex == "total", race == "total") %>%
+      left_join(test1, by = c("zip", "year", "race", "sex")) %>%
+      transmute(zip, poverty, year,
+                population_in_Louisville = population_in_MSA,
+                population_total,
+                percent_in_Louisville = population_in_Louisville / population_total * 100) %>%
+      left_join(test4) %>%
+      left_join(MSA_FIPS_info) %>%
+      filter(percent_in_Louisville > 50) %>%
+      select(zip, year, poverty, county, population_in_Louisville, population_total, percent_in_Louisville)
 
-  df
-}
-process_05  <- function(df, geog){
+   write_csv(test3, "poverty_in_Louisville_2019.csv")
 
-  col_name <- switch(geog, "FIPS" = "FIPS", "MSA" = "Id2", "tract" = "Id")
-
-  df %>%
-    transmute(
-      !!geog := as.character(.data[[col_name]]),
-      year,
-
-      num_pov_under_5.male =
-        `Estimate; Income in the past 12 months below poverty level: - Male: - Under 5 years`,
-      num_pov_under_5.female =
-        `Estimate; Income in the past 12 months below poverty level: - Female: - Under 5 years`,
-      num_pov_under_5 =
-        num_pov_under_5.male + num_pov_under_5.female,
-
-      num_pov_5_17.male =
-        `Estimate; Income in the past 12 months below poverty level: - Male: - 5 years` +
-        `Estimate; Income in the past 12 months below poverty level: - Male: - 6 to 11 years` +
-        `Estimate; Income in the past 12 months below poverty level: - Male: - 12 to 14 years` +
-        `Estimate; Income in the past 12 months below poverty level: - Male: - 15 years` +
-        `Estimate; Income in the past 12 months below poverty level: - Male: - 16 and 17 years`,
-      num_pov_5_17.female =
-        `Estimate; Income in the past 12 months below poverty level: - Female: - 5 years` +
-        `Estimate; Income in the past 12 months below poverty level: - Female: - 6 to 11 years` +
-        `Estimate; Income in the past 12 months below poverty level: - Female: - 12 to 14 years` +
-        `Estimate; Income in the past 12 months below poverty level: - Female: - 15 years` +
-        `Estimate; Income in the past 12 months below poverty level: - Female: - 16 and 17 years`,
-      num_pov_5_17 =
-        num_pov_5_17.male + num_pov_5_17.female,
-
-      num_pov_child.male =
-        num_pov_under_5.male + num_pov_5_17.male,
-      num_pov_child.female =
-        num_pov_under_5.female + num_pov_5_17.female,
-      num_pov_child =
-        num_pov_child.male + num_pov_child.female,
-
-      num_pov.male =
-        `Estimate; Income in the past 12 months below poverty level: - Male:`,
-      num_pov.female =
-        `Estimate; Income in the past 12 months below poverty level: - Female:`,
-      num_pov =
-        num_pov.male + num_pov.female,
-
-      total_under_5.male =
-        num_pov_under_5.male +
-        `Estimate; Income in the past 12 months at or above poverty level: - Male: - Under 5 years`,
-      total_under_5.female =
-        num_pov_under_5.female +
-        `Estimate; Income in the past 12 months at or above poverty level: - Female: - Under 5 years`,
-      total_under_5 =
-        total_under_5.male + total_under_5.female,
-
-      total_5_17.male =
-        num_pov_5_17.male +
-        `Estimate; Income in the past 12 months at or above poverty level: - Male: - 5 years` +
-        `Estimate; Income in the past 12 months at or above poverty level: - Male: - 6 to 11 years` +
-        `Estimate; Income in the past 12 months at or above poverty level: - Male: - 12 to 14 years` +
-        `Estimate; Income in the past 12 months at or above poverty level: - Male: - 15 years` +
-        `Estimate; Income in the past 12 months at or above poverty level: - Male: - 16 and 17 years`,
-      total_5_17.female =
-        num_pov_5_17.female +
-        `Estimate; Income in the past 12 months at or above poverty level: - Female: - 5 years` +
-        `Estimate; Income in the past 12 months at or above poverty level: - Female: - 6 to 11 years` +
-        `Estimate; Income in the past 12 months at or above poverty level: - Female: - 12 to 14 years` +
-        `Estimate; Income in the past 12 months at or above poverty level: - Female: - 15 years` +
-        `Estimate; Income in the past 12 months at or above poverty level: - Female: - 16 and 17 years`,
-      total_5_17 =
-        total_5_17.male + total_5_17.female,
-
-      total_child.male =
-        total_under_5.male + total_5_17.male,
-      total_child.female =
-        total_under_5.female + total_5_17.female,
-      total_child =
-        total_child.male + total_child.female,
-
-      total.male =
-        num_pov.male +
-        `Estimate; Income in the past 12 months at or above poverty level: - Male:`,
-      total.female =
-        num_pov.female +
-        `Estimate; Income in the past 12 months at or above poverty level: - Female:`,
-      total =
-        total.male + total.female)
-}
-process_pov <- function(df, race_name){
-
-  if (df_type(df) == "MSA") geog <- "MSA" else geog <- "FIPS"
-
-  df %>%
-
-    #filter to peers
-    pull_peers(add_info = FALSE) %>%
-
-    #gather data, separate sex into column, replace NAs with total, and spread
-    reshape_sex() %>%
-
-    #add race
-    mutate(race = race_name) %>%
-
-    #reorder variables
-    select(
-      !!geog, year, race, sex,
-      total, total_child, total_under_5, total_5_17,
-      num_pov, num_pov_child, num_pov_under_5, num_pov_5_17) %>%
-
-    #merge STL counties
-    {if (geog == "FIPS") stl_merge(., total:num_pov_5_17, method = "sum") else .} %>%
-
-    #calculate percentages
-    mutate(
-      poverty = num_pov / total * 100,
-      child_poverty = num_pov_child / total_child * 100,
-      child_poverty_under_5 = num_pov_under_5 / total_under_5 * 100,
-      child_poverty_5_17 = num_pov_5_17 / total_5_17 * 100) %>%
-
-    #remove unnecessary variables
-    select(-contains("num"), -contains("total"))
 }
 
-#Total
-poverty_00      <- read_csv(path %p% "DEC_00_SF3_PCT049_with_ann.csv", skip = 1)
-poverty_05      <- acs_time(path %p% "B17001")
-poverty_map     <- read_csv(path %p% "ACS_17_5YR_B17001_with_ann.csv", skip = 1)
-poverty_msa_5yr <- acs_time(path %p% "B17001_5yr", geography = "MSA", starting_year = 2007)
-poverty_msa_1yr <- read_csv(path %p% "B17001_msa/ACS_17_1YR_B17001_with_ann.csv", skip = 1)
+process_map(poverty_map, poverty:poverty_under_18, return_name = "poverty") %>% list2env(.GlobalEnv)
 
-poverty_00      %<>% process_00()
-poverty_05      %<>% process_05("FIPS")
-poverty_map     %<>% mutate(year = 2015) %>% process_05("tract")
-poverty_00_msa  <- poverty_00 %>% sum_FIPS_to_MSA()
-poverty_msa_5yr %<>% process_05("FIPS") %>% sum_FIPS_to_MSA()
-poverty_msa_1yr %<>% mutate(year = 2017) %>% process_05("MSA")
+# Let's look at the # of kids under each % of poverty
+if(FALSE) {
+  acs_micro <- feather::read_feather("data-raw/microdata/acs_micro_repwts.feather")
 
-poverty_tot <- bind_rows(poverty_00, poverty_05)
+  acs_micro %<>%
+     filter(age == 4, FIPS == "21111")
 
-poverty_tot     %<>% process_pov("total")
-poverty_msa_5yr %<>% bind_rows(poverty_00_msa, .) %>% process_pov("total")
-poverty_msa_1yr %<>% bind_rows(poverty_00_msa, .) %>% process_pov("total")
+  temp <- acs_micro %>%
+     filter(FIPS == "21111") %>%
+     mutate(poverty = POVERTY < 150) %>%
+     survey_by_demog("poverty",
+                     type = "categorical",
+                     breakdowns = c("total", "race"))
 
-poverty_tract <- poverty_map %>%
-  transmute(
-    tract,
-    year,
-    poverty = num_pov / total * 100,
-    child_poverty = num_pov_child / total_child * 100,
-    child_poverty_under_5 = num_pov_under_5 / total_under_5 * 100,
-    child_poverty_5_17 = num_pov_5_17 / total_5_17 * 100)
+  temp2 <- acs_micro %>%
+     filter(FIPS == "21111", year %in% 2019) %>%
+     mutate(poverty = POVERTY < 150) %>%
+     svy_bootstrap(var = "poverty",
+                   weight_var = "PERWT",
+                   type = "categorical",
+                   grouping_vars = "FIPS")
 
-poverty_nh <- poverty_map %>%
-  left_join(nh_tract, by = c("tract" = "GEO_ID")) %>%
-  group_by(neighborhood, year) %>%
-  summarise(
-    poverty = sum(num_pov) / sum(total) * 100,
-    child_poverty = sum(num_pov_child) / sum(total_child) * 100,
-    child_poverty_under_5 = sum(num_pov_under_5) / sum(total_under_5) * 100,
-    child_poverty_5_17 = sum(num_pov_5_17) / sum(total_5_17) * 100) %>%
-  ungroup()
+  temp %<>%
+     filter(race == "total")
 
-#White
-poverty_00_white <- read_csv(path %p% "DEC_00_SF3_PCT075I_with_ann.csv", skip = 1)
-poverty_05_white <- acs_time(path %p% "B17001H")
+  write_csv(temp, "child_poverty.csv")
 
-poverty_00_white %<>% process_00()
-poverty_05_white %<>% process_05("FIPS")
+  pov_threshold <- function(pct) {
+     acs_micro %>%
+        filter(FIPS == "21111", year == 2019) %>%
+        mutate(poverty = POVERTY <= pct) %>%
+        survey_by_demog("poverty", method="bootstrap", breakdowns = c("total", "race")) %>%
+        mutate(threshold = pct)
+  }
 
-poverty_white <- bind_rows(poverty_00_white, poverty_05_white)
+  library(future)
+  plan("multisession")
 
-poverty_white %<>% process_pov(race_name = "white")
+  test <- map_dfr(seq(5, 500, by=1),
+                                ~pov_threshold(.))
 
+  graph_df <- test %>%
+     filter(race %in% c("black", "total"), var_type %in% c("percent.estimate", "percent.lower", "percent.upper")) %>%
+     pivot_wider(values_from = poverty, names_from = "var_type")
 
-#Black
-poverty_00_black <- read_csv(path %p% "DEC_00_SF3_PCT075B_with_ann.csv", skip = 1)
-poverty_05_black <- acs_time(path %p% "B17001B")
-
-poverty_00_black %<>% process_00()
-poverty_05_black %<>% process_05("FIPS")
-
-poverty_black <- bind_rows(poverty_00_black, poverty_05_black)
-
-poverty_black %<>% process_pov(race_name = "black")
+  ggplot(graph_df, aes(x=threshold, y=percent.estimate, group=race, color=race)) +
+      geom_line() +
+      geom_point()+
+      geom_errorbar(aes(ymin=percent.lower, ymax=percent.upper))
+}
 
 
-#Hispanic
-poverty_00_hispanic <- read_csv(path %p% "DEC_00_SF3_PCT075H_with_ann.csv", skip = 1)
-poverty_05_hispanic <- acs_time(path %p% "B17001I")
+usethis::use_data(poverty_county, poverty_tract, poverty_nh, poverty_muw, overwrite = TRUE)
 
-poverty_00_hispanic %<>% process_00()
-poverty_05_hispanic %<>% process_05("FIPS")
-
-poverty_hispanic <- bind_rows(poverty_00_hispanic, poverty_05_hispanic)
-
-poverty_hispanic %<>% process_pov(race_name = "hispanic")
-
-#combine data frames
-poverty_county <- bind_rows(poverty_tot, poverty_white, poverty_black, poverty_hispanic)
-
-update_sysdata(poverty_county, poverty_msa_1yr, poverty_msa_5yr, poverty_tract, poverty_nh)
-
-rm(path, process_00, process_05, process_pov, poverty_map,
-   poverty_00, poverty_05, poverty_tot,
-   poverty_00_white, poverty_05_white, poverty_white,
-   poverty_00_black, poverty_05_black, poverty_black,
-   poverty_00_hispanic, poverty_05_hispanic, poverty_hispanic)
+rm(poverty_vars_00_total, poverty_vars_00_race, poverty_vars_05_1yr, poverty_vars_05_5yr,
+   poverty_vars_1yr, poverty_vars_5yr,
+   poverty_map)
