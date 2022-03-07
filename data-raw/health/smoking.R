3d687dc8bae19911d4faae7689f2eb58d1cf661d
mazdun <matthew.zdun@gmail.com>
2022-03-07 13:49:38 -0600

    Smoking data

---
+++ data-raw/health/smoking.R
@@ -0,0 +1,15 @@
+suppressMessages(library(tidyverse))
+library(glptools)
+
+path <- "data-raw/health/smoking/"
+
+#Gather data
+smoking_county <- read.csv(paste0(path, 'smoking_total/PLACES_2021.csv')) %>%
+  filter(Measure=='Current smoking among adults aged >=18 years' & Data_Value_Type=='Age-adjusted prevalence') %>%
+  select(c('LocationID','Year','Data_Value')) %>%
+  mutate(sex='total',race='total') %>%
+  rename('FIPS'='LocationID', 'year'='Year', 'smoking_percentage'='Data_Value') %>%
+  glptools::pull_peers(add_info = FALSE, subset_to_peers = TRUE, geog="FIPS")
+
+usethis::use_data(smoking_county, overwrite = TRUE)
+
