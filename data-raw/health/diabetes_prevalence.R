cb401dcbe07cf73569a2bb60a53dbbd0fd1edbff
mazdun <matthew.zdun@gmail.com>
2022-03-06 20:51:53 -0600

    Obesity county data

--- data-raw/health/diabetes_prevalence.R
+++ data-raw/health/diabetes_prevalence.R
@@ -9,32 +9,27 @@ diabetes_df_total <- glptools::any_time(paste0(path, 'total'), starting_year=200
 #Process and subset data
 diabetes_df_total <- diabetes_df_total %>%
   select(c('CountyFIPS','year','Percentage')) %>%
-  mutate(sex='All',race='All') %>%
+  mutate(sex='total',race='total') %>%
   rename('FIPS'='CountyFIPS','diabetes_prevalence'='Percentage') %>%
   glptools::pull_peers(add_info = FALSE, subset_to_peers = TRUE, geog="FIPS")
 
 #Male
 diabetes_df_male <- glptools::any_time(paste0(path, 'male'), starting_year=2004, skip=0, col_types=NULL,read.csv) %>%
   select(c('CountyFIPS','year','Percentage')) %>%
-  mutate(sex='male',race='All') %>%
+  mutate(sex='male',race='total') %>%
   rename('FIPS'='CountyFIPS','diabetes_prevalence'='Percentage') %>%
   glptools::pull_peers(add_info = FALSE, subset_to_peers = TRUE, geog="FIPS")
 
 #Female
 diabetes_df_female <- glptools::any_time(paste0(path, 'female'), starting_year=2004, skip=0, col_types=NULL,read.csv) %>%
   select(c('CountyFIPS','year','Percentage')) %>%
-  mutate(sex='female',race='All') %>%
+  mutate(sex='female',race='total') %>%
   rename('FIPS'='CountyFIPS','diabetes_prevalence'='Percentage') %>%
   glptools::pull_peers(add_info = FALSE, subset_to_peers = TRUE, geog="FIPS")
 
 #Combine all of the data
 diabetes_prevalence_county <- rbind(diabetes_df_total, diabetes_df_male, diabetes_df_female)
 diabetes_prevalence_county$diabetes_prevalence <- as.numeric(diabetes_prevalence_county$diabetes_prevalence)
-
-#Replace "All" in the race and sex columns with "total" to be able to use GLP graphic functions later
-diabetes_prevalence_county <- diabetes_prevalence_county %>% mutate(race=replace(race, race=='All', 'total'), sex=replace(sex, sex=='All', 'total'))
-diabetes_prevalence_county$race <- tolower(diabetes_prevalence_county$race)
-diabetes_prevalence_county$sex <- tolower(diabetes_prevalence_county$sex)
 
 usethis::use_data(diabetes_prevalence_county, overwrite = TRUE)
 
---
+++ data-raw/health/obesity.R
@@ -0,0 +1,34 @@
+suppressMessages(library(tidyverse))
+library(glptools)
+
+path <- "data-raw/health/obesity/"
+
+#Gather data
+obesity_df_total <- glptools::any_time(paste0(path, 'total'), starting_year=2004, skip=0, col_types=NULL,read.csv)
+
+#Process and subset data
+obesity_df_total <- obesity_df_total %>%
+  select(c('CountyFIPS','year','Percentage')) %>%
+  mutate(sex='total',race='total') %>%
+  rename('FIPS'='CountyFIPS','obesity_percentage'='Percentage') %>%
+  glptools::pull_peers(add_info = FALSE, subset_to_peers = TRUE, geog="FIPS")
+
+#Male
+obesity_df_male <- glptools::any_time(paste0(path, 'male'), starting_year=2004, skip=0, col_types=NULL,read.csv) %>%
+  select(c('CountyFIPS','year','Percentage')) %>%
+  mutate(sex='male',race='total') %>%
+  rename('FIPS'='CountyFIPS','obesity_percentage'='Percentage') %>%
+  glptools::pull_peers(add_info = FALSE, subset_to_peers = TRUE, geog="FIPS")
+
+#Female
+obesity_df_female <- glptools::any_time(paste0(path, 'female'), starting_year=2004, skip=0, col_types=NULL,read.csv) %>%
+  select(c('CountyFIPS','year','Percentage')) %>%
+  mutate(sex='female',race='total') %>%
+  rename('FIPS'='CountyFIPS','obesity_percentage'='Percentage') %>%
+  glptools::pull_peers(add_info = FALSE, subset_to_peers = TRUE, geog="FIPS")
+
+#Combine all of the data
+obesity_county <- rbind(obesity_df_total, obesity_df_male, obesity_df_female)
+obesity_county$obesity_percentage <- as.numeric(obesity_county$obesity_percentage)
+
+usethis::use_data(obesity_county, overwrite = TRUE)
